7bbbbeb4cd09f6fd6bb7479f31d8234da6132fea
SamerMakni <dmc4samer@gmail.com>
2022-02-14 23:13:48 +0100

    Added Intitial Background Color Features

--- script.js
+++ script.js
@@ -25,7 +25,28 @@ function crosser() {
 
 }
 
+function backgroundLoader() {
+    let apiUrl = "https://api.smk.dk/api/v1/art/search/?keys=*&fields=image_thumbnail,colors,artist,titles,labels&filters=[has_image:true],[object_names:maleri],[public_domain:true]&offset=0&rows=300";
+    fetch(apiUrl).then(response => {
+        return response.json();
+    }).then(data => {
+        let random = Math.floor(Math.random() * 300);
+        console.log(data.items[random].image_thumbnail);
+        let body = document.getElementById("myBody");
+        document.getElementById("leftSection").classList.add("bg-[" + data.items[random].colors[1] + "]");
+        console.log(data.items[random].colors[1]);
+
+        body.style.backgroundImage = "url('" + data.items[random].image_thumbnail + "')";
+
+
+    }).catch(err => {
+        console.log("API Error.")
+    });
+}
+
+
 window.onload = function() {
     dateLoader();
     quoteLoader();
+    backgroundLoader();
 }
