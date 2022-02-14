51a5688535ea498f99d4234e4d50ce81ae8b0832
SamerMakni <dmc4samer@gmail.com>
2022-02-15 00:38:14 +0100

    Added Artwork Reference

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function crosser() {
 }
 
 function backgroundLoader() {
-    let apiUrl = "https://api.smk.dk/api/v1/art/search/?keys=*&fields=image_thumbnail,colors,artist,titles,labels&filters=[has_image:true],[object_names:maleri],[public_domain:true]&offset=0&rows=300";
+    let apiUrl = "https://api.smk.dk/api/v1/art/search/?keys=*&fields=image_thumbnail,colors,artist,titles,labels,object_number&filters=[has_image:true],[object_names:maleri],[public_domain:true]&offset=0&rows=300";
     fetch(apiUrl).then(response => {
         return response.json();
     }).then(data => {
@@ -35,8 +35,14 @@ function backgroundLoader() {
         let body = document.getElementById("myBody");
         document.getElementById("leftSection").classList.add("bg-[" + data.items[random].colors[1] + "]");
         console.log(data.items[random].colors[1]);
-
+        document.getElementById("rightSection").classList.add("bg-[" + data.items[random].colors[2] + "]");
+        console.log(data.items[random].colors[2])
+        let text = data.items[random].titles[0].title + " by " + data.items[random].artist;
+        let link = data.items[random].object_number;
+        link = "https://open.smk.dk/artwork/image/" + link.toLowerCase();
+        document.getElementById("reference").innerHTML = `<a href="${link}" target="_blank">${text}</a>`;
         body.style.backgroundImage = "url('" + data.items[random].image_thumbnail + "')";
+
 
 
     }).catch(err => {
@@ -46,7 +52,7 @@ function backgroundLoader() {
 
 
 window.onload = function() {
+    backgroundLoader();
     dateLoader();
     quoteLoader();
-    backgroundLoader();
 }
