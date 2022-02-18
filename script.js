4a9acbc9f72475d1cfda3e1f2328732c53de55f8
SamerMakni <dmc4samer@gmail.com>
2022-02-18 15:18:28 +0100

    Added Colors to Modal, Added input control

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function colorizer(str) {
     return intensity > 168 ? true : false;
 }
 
-
+i = 10;
 
 
 function dateLoader() {
@@ -49,24 +49,34 @@ function backgroundLoader() {
         document.getElementById("rightSection").classList.add("bg-[" + data.items[random].colors[2] + "]");
         console.log(data.items[random].colors[2])
         let text = data.items[random].titles[0].title + " by " + data.items[random].artist;
+        body.style.backgroundImage = "url('" + data.items[random].image_thumbnail + "')";
         let link = data.items[random].object_number;
         link = "https://open.smk.dk/artwork/image/" + link.toLowerCase();
         document.getElementById("reference").innerHTML = `<a href="${link}" target="_blank">${text}</a>`;
-        body.style.backgroundImage = "url('" + data.items[random].image_thumbnail + "')";
         if (colorizer(data.items[random].colors[1]) == false)
             document.getElementById("leftSection").classList.add("text-white");
-        if (colorizer(data.items[random].colors[2]) == false)
+        if (colorizer(data.items[random].colors[2]) == false) {
             document.getElementById("rightSection").classList.add("text-white");
+        }
+        document.getElementById("modalContent").classList.add("bg-[" + data.items[random].colors[3] + "]");
+        document.getElementById("input").classList.add("bg-[" + data.items[random].colors[3] + "]");
+        if (colorizer(data.items[random].colors[3]) == false) {
+            document.getElementById("input").classList.add("border-white");
+            document.getElementById("input").classList.add("text-white");
+            document.getElementById("accept").classList.add("text-white");
 
+        }
 
     }).catch(err => {
-        console.log("API Error.")
+        console.log(err);
     });
 }
 
 document.getElementById("add").addEventListener("click", function() {
-    document.getElementById("tasks").innerHTML += '<br> <input class="form-check-input appearance-none h-3 w-3 border border-gray-300 rounded-sm bg-gray-300 checked:bg-black checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"   type="checkbox" value="" id="flexCheckDefault6">    <label class="form-check-label inline-block" for="flexCheckDefault6"> test  </label>';
-
+    document.getElementById("myModal").classList.remove("hidden");
+    document.getElementById("myModal").classList.add("block");
+    document.getElementById("error").innerHTML = "";
+    errr = false;
 })
 
 window.onload = function() {
@@ -74,3 +84,27 @@ window.onload = function() {
     dateLoader();
     quoteLoader();
 }
+window.onclick = function(event) {
+    if (event.target == document.getElementById("myModal")) {
+        document.getElementById("myModal").classList.add("hidden");
+
+
+    }
+}
+let errr = false;
+
+document.getElementById("accept").addEventListener("click", function() {
+
+    let newTask = document.getElementById("input").value;
+    if (newTask.length == 0) {
+        if (errr == false) document.getElementById("error").innerHTML += "No task entered";
+        errr = true;
+
+
+    } else {
+        document.getElementById("tasks").innerHTML += '<br> <input class="form-check-input appearance-none h-3 w-3 border border-gray-300 rounded-sm bg-gray-300 checked:bg-black checked:border-black focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"   type="checkbox" value="" id="flexCheckDefault' + i + '">    <label class="form-check-label inline-block" for="flexCheckDefault' + i + '"> ' + newTask + ' </label>';
+        i++;
+        document.getElementById("myModal").classList.add("hidden");
+        console.log(i);
+    }
+});
